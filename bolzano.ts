7a8f98e2f094f7344980057fc18b85b4aea16274
Facundo Agustín Fernández <facuassain@gmail.com>
2021-10-31 19:58:28 -0300

    Funcion de ejemplo cambiada

--- bolzano.ts
+++ bolzano.ts
@@ -1,17 +1,22 @@
 // Función a calcular las raíces
-const f = (x : number) : number => Math.exp(x) - 4 + x
+const f = (x : number) : number => Math.exp(x/2) - Math.pow(x,2) - 3*x
 
 const biseccion = (a : number,b : number,f : ( (x : number) => number ) ) : (number | undefined) => {
 	if( f(a) * f(b) < 0){
 		let xn = (a + b)/2
 		console.log(xn)
 		// Criterio de corte utilizado. Modificar el Math.pow() para cambiar el error admitido
-		if( Math.abs( f(xn) ) <= Math.pow(10,-2) ) return xn
+		if( Math.abs( f(xn) ) <= Math.pow(10,-5) ) return xn
 		Math.sign( f(xn) ) === Math.sign( f(a) ) ? a = xn : b = xn
 		return biseccion(a,b,f)
+	}else {
+		console.log('Problema con los signos')
+		return undefined
 	}
 	
 }	
 
-const res = biseccion(1,2,f)
-console.log('RESULTADO DESPUES DE LA ITERACIÓN',res)
+const resultadoPrimerIntervalo= biseccion(-4,-2,f)
+const resultadoSegundoIntervalo = biseccion(0,0.5,f)
+console.log('RESULTADO APROXIMADO DE LA PRIMER RAÍZ DESPUES DE LA ITERACIÓN',resultadoPrimerIntervalo)
+console.log('RESULTADO APROXIMADO DE LA SEGUNDA RAÍZ DESPUES DE LA ITERACIÓN',resultadoSegundoIntervalo)
